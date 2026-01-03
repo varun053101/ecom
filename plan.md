f7b5b24b5bf4087852f4676e0b5af4173fa1ae86
varun053101 <varun05310104@gmail.com>
2026-01-04 01:32:24 +0530

    Created Data Model

---
+++ plan.md
@@ -0,0 +1,19 @@
+# Product
+`id` -> Primary Key \
+`name` -> String \
+`description` -> String \
+`brand` -> String \
+`price` -> Integer \
+`Category` -> enum \
+`release date` -> Date  
+`available` -> Boolean
+`Image` -> File
+
+# Packages
+`Controller` -> ProductController \
+`Service` \
+`Model` \
+`Repo`
+
+
+
---
+++ src/main/java/com/project/ecom/model/Product.java
@@ -0,0 +1,28 @@
+package com.project.ecom.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
+
+}
